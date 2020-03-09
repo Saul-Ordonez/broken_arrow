78dc4ddd63b0a72d2d2f2cf20dfaf98f42bd98fa
Saul Ordonez <sauleordonez@gmail.com>
2020-03-09 16:53:13 -0700

    add navbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Landing from './Landing';
+import constants from './../constants';
+const { c } = constants;
 
 function App() {
   return (
---
+++ src/constants/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD6jWR2uwixFL1Kt8c7S2zEa0yRSIGMK5c",
+  authDomain: "broken-arrow-users.firebaseapp.com",
+  databaseURL: "https://broken-arrow-users.firebaseio.com",
+  projectId: "broken-arrow-users",
+  storageBucket: "broken-arrow-users.appspot.com",
+  messagingSenderId: "76243625629",
+  appId: "1:76243625629:web:9b8786f372ec7b82cd9674",
+  measurementId: "G-EH0QSW4GMK"
+};
+
+export default firebaseConfig;
