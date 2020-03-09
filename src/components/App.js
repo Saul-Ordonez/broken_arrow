ee13d644a60b68d0a3027057b39d3c2d53341325
Saul Ordonez <sauleordonez@gmail.com>
2020-03-09 09:44:19 -0700

    add routing to app.js and update brower tab

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Landing from './Landing';
 
 function App() {
   return (
     <div>
-
+    <Switch>
+    <Route exact path='/' component={Landing} />
+    </Switch>
     </div>
   );
 }
